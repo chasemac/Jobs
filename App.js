7187499bf1b4e0cd255c36b4efa51b1d49ab3d25
Chase McElroy <ichasem@gmail.com>
2018-04-25 10:03:14 -0700

    Setup redux

--- App.js
+++ App.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { TabNavigator, StackNavigator } from 'react-navigation';
+import { Provider } from 'react-redux';
 
+import store from './store';
 import AuthScreen from './screens/AuthScreen';
 import WelcomeScreen from './screens/WelcomeScreen';
 import MapScreen from './screens/MapScreen';
@@ -38,7 +40,9 @@ export default class App extends React.Component {
     // });
     // console.log(MainNavigator)
 
-    return <RootStack/>;
+    return <Provider store={store}>
+            <RootStack/>
+          </Provider> ;
   }
 }
 
