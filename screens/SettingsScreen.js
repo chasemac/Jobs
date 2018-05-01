e1d9bf12d5b20e89c255a59845e7fc036b99ceef
Chase McElroy <ichasem@gmail.com>
2018-05-01 16:58:49 -0700

    Created logout button

--- screens/SettingsScreen.js
+++ screens/SettingsScreen.js
@@ -1,11 +1,24 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, AsyncStorage } from 'react-native';
 import { Button } from 'react-native-elements';
 import { connect } from 'react-redux';
 import * as actions from '../actions';
 
 class SettingsScreen extends Component {
+
     render() {
+        async function logout() {
+            try {
+                await AsyncStorage.removeItem('fb_token')
+                console.log('====================================');
+                console.log("LOGGED OUT");
+                console.log('====================================');
+            } catch (error) {
+                console.log('==============ERROR======================');
+                console.log(error);
+                console.log('==============ERROR END======================');
+            }
+        }
         return (
             <View>
                 <Button 
@@ -14,6 +27,19 @@ class SettingsScreen extends Component {
                     icon={{ name: 'delete-forever' }}
                     backgroundColor="#F44336"
                     onPress={this.props.clearLikedJobs}
+                />
+                <View
+                style={{
+                    height: 100,
+                    padding: 20,}}
+                >
+                </View>
+                <Button 
+                    title="Log Out"
+                    large
+                    icon={{ name: 'delete-forever' }}
+                    backgroundColor="#F44336"
+                    onPress={logout.bind(this)}
                 />
             </View>
         );
